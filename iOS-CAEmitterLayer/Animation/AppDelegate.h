311ea3822d23c0663aea19266af1564a4dc4736c
lizhen01 <1305766898@qq.com>
2017-04-27 14:18:23 +0800

    Add files via upload

---
+++ iOS-CAEmitterLayer/Animation/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  Animation
+//
+//
+//  Created by lz on 17/4/27.
+//  Copyright © 2017年 lz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS-CAEmitterLayer/Animation/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Animation
+//
+//
+//  Created by lz on 17/4/27.
+//  Copyright © 2017年 lz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
